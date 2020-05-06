c1c6197627d1dc5f60af58151b9956c48e20c600
Artem Bondarets <tyom.bond@gmail.com>
2020-05-06 10:40:03 +0300

    Update sets generator

--- generate_sets.rb
+++ generate_sets.rb
@@ -1,23 +1,23 @@
 require 'rubystats'
 
-sizes = [30_000, 100_000, 300_000, 1_000_000]
+sizes = [3_000, 10_000, 30_000, 100_000]
 
 5.times do |version|
   sizes.each_with_index do |size, size_index|
 
-    File.open("./sets/#{size_index+1}size_1range_#{version+1}version", "a") do |file|
+    File.open("./sets/1range_#{size_index+1}size_#{version+1}version", "a") do |file|
       size.times { file.puts rand(2**31+1) }
     end
 
-    File.open("./sets/#{size_index+1}size_2range_#{version+1}version", "a") do |file|
+    File.open("./sets/2range_#{size_index+1}size_#{version+1}version", "a") do |file|
       size.times { file.puts rand(size) }
     end
 
-    File.open("./sets/#{size_index+1}size_3range_#{version+1}version", "a") do |file|
+    File.open("./sets/3range_#{size_index+1}size_#{version+1}version", "a") do |file|
       size.times { file.puts rand(2**15+1) }
     end
 
-    File.open("./sets/#{size_index+1}size_4range_#{version+1}version", "a") do |file|
+    File.open("./sets/4range_#{size_index+1}size_#{version+1}version", "a") do |file|
       gen = Rubystats::NormalDistribution.new(2**30, 100)
       file.puts gen.rng(size-1).map(&:to_i)
     end
